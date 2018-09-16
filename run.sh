4b0e787b6c79cf5c273187647a4daa599206f67c
Gordon Young <gyoung@lpt-C02TP316HF1R.corp.verygood.systems>
2018-09-16 13:53:50 -0700

    renamed:    Dockerfile-build.sh -> build.sh 	renamed:    example-run.sh -> run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker pull ictu/zap2docker-weekly
-docker run --rm -v $(pwd):/zap/wrk/:rw -t ictu/zap2docker-weekly zap-baseline-custom.py -r testreport.html -g gen.conf -d -m 5 \
+docker pull gjyoung1974/zap2docker-weekly
+docker run --rm -v $(pwd):/zap/wrk/:rw -t gjyoung1974/zap2docker-weekly zap-baseline-custom.py -r testreport.html -g gen.conf -d -m 5 \
 -t http://www.website.com \
---auth_loginurl "http://www.website.com/login" \
+--auth_loginurl "http://www.verygoodsecurity.com" \
 --auth_username exampleuser \
 --auth_password p@ssw0rd \
 --auth_usernamefield j_username \
