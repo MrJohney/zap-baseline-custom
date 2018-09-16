09782a337036ed5dff66a2d974d223e8cd6b15c3
Gordon Young <gyoung@lpt-C02TP316HF1R.corp.verygood.systems>
2018-09-16 13:58:24 -0700

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-docker pull gjyoung1974/zap2docker-weekly
 docker run --rm -v $(pwd):/zap/wrk/:rw -t gjyoung1974/zap2docker-weekly zap-baseline-custom.py -r testreport.html -g gen.conf -d -m 5 \
--t http://www.website.com \
+-t http://www.verygoodsecurity.com \
 --auth_loginurl "http://www.verygoodsecurity.com" \
 --auth_username someuser \
 --auth_password p@ssw0rd \
