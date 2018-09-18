b9dbb06a361421ed24d56488f7fa7d617ea2e1dd
Gordon Young <gjyoung1974@gmail.com>
2018-09-18 10:55:47 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 docker run --rm -v $(pwd):/zap/wrk/:rw -t zap-baseline-custom zap-baseline-custom.py -r testreport.html -g gen.conf -d -m 5 \
--t https://dashboard.verygoodsecurity.com \
---auth_loginurl "https://{some-tenant}.auth0.com/login?state=..." \
---auth_username "gjyoung1974@gmail.com" \
---auth_password "SomePassword" \
---auth_usernamefield j_username \
---auth_passwordfield j_password \
---auth_submitfield submit \
---auth_exclude "https://dashboard.example.com/j_spring_security_logout,https://dashboard.example.com/j_spring_security_check.*" \
---active_scan
+ -t http://www.example.com # \
+# --auth_loginurl "https://{some-tenant}.auth0.com/login?state=..." \
+# --auth_username "gjyoung1974@gmail.com" \
+# --auth_password "SomePassword" \
+# --auth_usernamefield j_username \
+# --auth_passwordfield j_password \
+# --auth_submitfield submit \
+# --auth_exclude "https://dashboard.example.com/j_spring_security_logout,https://dashboard.example.com/j_spring_security_check.*" \
+# --active_scan
+
+# Uncomment the above to specificy credentials parameters ^^^
